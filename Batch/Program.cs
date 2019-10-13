331ddfb60615eddd0fcfed0d70eab10e9916bdf8
kazenetu <rakudafactory@gmail.com>
2019-10-13 17:32:43 +0900

    ダミー出力を追加

--- Batch/Program.cs
+++ Batch/Program.cs
@@ -10,17 +10,24 @@ namespace Batch
   {
     static void Main(string[] args)
     {
-      // 最大値
+      // 最大カウント数
       var maxCount = 10;
       Console.WriteLine($"MaxCount:{maxCount}");
 
       // 実測値
       for (int i = 0; i <= maxCount; i++)
       {
+        // 現在カウント数
         Console.WriteLine($"NowCount:{i}");
+
+        // ダミーメッセージ
+        Console.WriteLine($"Dummy:{i}");
 
         // 0.5秒待つ
         System.Threading.Thread.Sleep(500);
+
+        // ダミーメッセージ
+        Console.WriteLine($"AAAAAAAA");
       }
     }
   }
